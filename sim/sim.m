a5951872e631d634db111341bbd5a13a09050366
Pioooooo <bowen.sun@qq.com>
2021-01-09 18:00:12 +0800

    update

--- sim/sim.m
+++ sim/sim.m
@@ -1,20 +1,20 @@
 clc
 clear
 %% t
-df_t=diaSim(5000,2,1);
-figure
-hold on
-plot((1:size(df_t,1))',df_t);
-grid on
-xlabel('T');
-ylabel('df');
-title('df-时间关系图');
-hold off
+% df_t=diaSim(5000,2,1);
+% figure
+% hold on
+% plot((1:size(df_t,1))',df_t);
+% grid on
+% xlabel('T');
+% ylabel('df');
+% title('df-时间关系图');
+% hold off
 %% n
 n=(500:500:50000)';
 df_n=zeros(size(n,1));
-for i=n
-    df1=diaSim(i,2,1);
+for i=1:length(n)
+    df1=diaSim(n(i),2,1);
     df_n(i)=df1(size(df1,1));
 end
 figure
@@ -28,8 +28,8 @@ hold off
 %% step
 step=(1:0.1:3)';
 df_st=zeros(size(step,1));
-for i=n
-    df1=diaSim(5000,i,1);
+for i=1:length(step)
+    df1=diaSim(5000,step(i),1);
     df_st(i)=df1(size(df1,1));
 end
 figure
@@ -43,8 +43,8 @@ hold off
 %%%% pr
 prob=(0.2:0.1:1)';
 df_pr=zeros(size(prob,1));
-for i=n
-    df1=diaSim(5000,2,i);
+for i=1:length(prob)
+    df1=diaSim(5000,2,prob(i));
     df_pr(i)=df1(size(df1,1));
 end
 figure
